8e8074bb3b2d2e7791a7dc6b4d9410b5f1d0c610
Leo Magsino <leo.magsino819@gmail.com>
2022-03-07 18:50:39 +0800

    updates returning checker

--- app/services/vehicle_manager/vehicle_returning_checker.rb
+++ app/services/vehicle_manager/vehicle_returning_checker.rb
@@ -2,16 +2,26 @@ module VehicleManager
 
   class VehicleReturningChecker < ApplicationService
 
+    SECONDS_IN_HOUR = 3600
+
+
+
     def initialize vehicle, transaction_time
       @vehicle = vehicle
       @transaction_time = transaction_time
     end
 
     def call
-      end_time = @vehicle.latest_parking_transaction.start_time
+      end_time = @vehicle.latest_parking_transaction.end_time
 
       return false if end_time > @transaction_time
-      end_time <= (@transaction_time - 1.hour)
+
+      transaction_time = Time.parse @transaction_time.to_s
+      end_time = Time.parse end_time.to_s
+
+      total_time_difference = (transaction_time - end_time)/SECONDS_IN_HOUR
+
+      total_time_difference <= SECONDS_IN_HOUR
     end
 
   end
